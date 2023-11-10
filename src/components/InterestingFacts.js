f6c833b56da43de6270c44e4fb7f2be0f251f91e
HadiAlMarzooq <ahcraft1@gmail.com>
2023-11-10 20:37:10 +0300

    Refactor InterestingFacts component to use collapsible sections

--- src/components/InterestingFacts.js
+++ src/components/InterestingFacts.js
@@ -1,73 +1,89 @@
-// src/components/InterestingFacts.js
 import {
     Box,
     Collapse,
     Flex,
     Heading,
     Icon,
-    List,
-    ListItem,
     Text,
     useDisclosure,
-} from "@chakra-ui/react";
-import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
-
-function InterestingFacts() {
-    const { isOpen, onToggle } = useDisclosure();
-    const facts = [
-        "Respiratory therapists care for patients who have trouble breathing—for example, from a chronic respiratory disease, such as asthma or emphysema.",
-        "Respiratory therapists need at least an associate’s degree, but employers may prefer applicants who have a bachelor’s degree.",
-        "Employment of respiratory therapists is projected to grow 19 percent from 2019 to 2029, much faster than the average for all occupations.",
-        "The median annual wage for respiratory therapists was $62,810 in May 2020.",
-        "Respiratory therapists are on the front lines of responding to respiratory emergencies and managing patients with chronic respiratory issues.",
-        "They also play a critical role in emergency situations and are a key part of the hospital response team.",
-        "Respiratory therapists work with patients of all ages, from premature infants whose lungs are not fully developed to elderly people whose lungs are diseased.",
+  } from "@chakra-ui/react";
+  import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
+  
+  function InterestingFacts() {
+    const mainDisclosure = useDisclosure();
+    const introDisclosure = useDisclosure();
+    const pathoDisclosure = useDisclosure();
+    const clinicalDisclosure = useDisclosure();
+    const treatmentDisclosure = useDisclosure();
+    const diagnosisDisclosure = useDisclosure();
+    const prognosisDisclosure = useDisclosure();
+  
+    const sections = [
+      { title: "Introduction", disclosure: introDisclosure },
+      { title: "Pathophysiology", disclosure: pathoDisclosure },
+      { title: "Clinical Manifestations", disclosure: clinicalDisclosure },
+      { title: "Treatment", disclosure: treatmentDisclosure },
+      { title: "Diagnosis", disclosure: diagnosisDisclosure },
+      { title: "Prognosis", disclosure: prognosisDisclosure },
     ];
-
+  
     return (
-        <Box mt={5}>
-            <Flex
+      <Box mt={5}>
+        <Flex
+          align="center"
+          justify="space-between"
+          onClick={mainDisclosure.onToggle}
+          cursor="pointer"
+          bg="primary.100"
+          p={4}
+          borderRadius="md"
+          boxShadow="md"
+          _hover={{
+            bg: "primary.200",
+          }}
+        >
+          <Heading size="lg" color="primary.500">
+            🔍 The Mystery Of Cystic Fibrosis
+          </Heading>
+          <Icon
+            as={mainDisclosure.isOpen ? ChevronUpIcon : ChevronDownIcon}
+            color="white"
+            boxSize={8}
+          />
+        </Flex>
+        <Collapse in={mainDisclosure.isOpen} animateOpacity>
+          {sections.map((section, index) => (
+            <Box key={index} mt={4}>
+              <Flex
                 align="center"
                 justify="space-between"
-                onClick={onToggle}
+                onClick={section.disclosure.onToggle}
                 cursor="pointer"
-                bg="primary.100"
-                p={4}
+                bg="primary.200"
+                p={3}
                 borderRadius="md"
                 boxShadow="md"
                 _hover={{
-                    bg: "primary.200",
+                  bg: "primary.400",
                 }}
-            >
-                <Heading size="lg" color="primary.500">
-                    🔍 Interesting Facts about RTs
+              >
+                <Heading size="md" color="primary.800">
+                  {section.title}
                 </Heading>
                 <Icon
-                    as={isOpen ? ChevronUpIcon : ChevronDownIcon}
-                    color="white"
-                    boxSize={8}
+                  as={section.disclosure.isOpen ? ChevronUpIcon : ChevronDownIcon}
+                  color="white"
+                  boxSize={6}
                 />
-            </Flex>
-            <Collapse in={isOpen} animateOpacity>
-                <List spacing={3} mt={4}>
-                    {facts.map((fact, index) => (
-                        <ListItem
-                            key={index}
-                            bg="white"
-                            p={4}
-                            borderRadius="md"
-                            boxShadow="md"
-                            _hover={{
-                                bg: "primary.50",
-                            }}
-                        >
-                            <Text color="primary.600">{fact}</Text>
-                        </ListItem>
-                    ))}
-                </List>
-            </Collapse>
-        </Box>
+              </Flex>
+              <Collapse in={section.disclosure.isOpen} animateOpacity>
+              </Collapse>
+            </Box>
+          ))}
+        </Collapse>
+      </Box>
     );
-}
-
-export default InterestingFacts;
+  }
+  
+  export default InterestingFacts;
+  
