6b010bae8e92addc2f9f0714588544b3a7879527
HadiAlMarzooq <ahcraft1@gmail.com>
2023-11-13 19:32:56 +0300

    Add React Router and Team component, remove unnecessary components and animations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import { Main } from "./components/Main";
 import {
   ChakraProvider,
-  Box,
   Container,
-  Heading,
   Text,
   extendTheme,
-  Badge,
   Flex,
   Link,
+  Icon,
 } from "@chakra-ui/react";
-import { Player } from "@lottiefiles/react-lottie-player";
-import TipsSection from "./components/TipsSection";
-import Quiz from "./components/Quiz";
-import ResourcesSection from "./components/ResourcesSection";
-import InterestingFacts from "./components/InterestingFacts";
-import { motion } from "framer-motion";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { FaLinkedin } from "react-icons/fa";
+import Team from "./components/Team";
 // Define your theme
 const theme = extendTheme({
-  fonts: {
+  fonts: { 
     heading: "Poppins, sans-serif",
     body: "Poppins, sans-serif",
     arabic: "Tajawal, sans-serif",
@@ -38,117 +33,31 @@ const theme = extendTheme({
   },
 });
 
-// Create motion variants for your components
-const MotionBox = motion(Box);
-const MotionHeading = motion(Heading);
-const MotionText = motion(Text);
-
 // Define your App component
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Container maxW="container.xl" padding={3}>
-        <MotionBox
-          p={5}
-          bg="primary.100"
-          borderRadius="md"
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
+      <Router>
+        <Container maxW="container.xl" padding={3}>
+          <Routes>
+            <Route exact path="/" element={<Main />} />
+            <Route path="/team" element={<Team />} />
+          </Routes>
+        </Container>
+        <Flex
+          textAlign="center"
+          paddingTop="0.5rem"
+          alignItems="center"
+          justifyContent="center"
         >
-          <MotionHeading
-            mb={3}
-            style={{
-              fontSize: "3rem",
-              fontWeight: "bold",
-              color: "#2D3748",
-              marginBottom: "1rem",
-            }}
-            color="primary.600"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.5, duration: 0.5 }}
-          >
-            🎉 Welcome Super RTs
-          </MotionHeading>
-          <MotionHeading
-            style={{
-              fontSize: "2rem",
-              fontWeight: "bold",
-              marginBottom: "1rem",
-            }}
-            mb={3}
-            color="primary.800"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.7, duration: 0.5 }}
-          >
-            Pediatrics Disorder Day at MACHS! 🫁
-          </MotionHeading>
-          <Flex justifyContent="center" mt={2}>
-            <Player
-              src="./lungs.json"
-              background="transparent"
-              speed={0.5}
-              style={{ width: "300px", height: "300px" }}
-              loop
-              autoplay
-              onError={(error) => {
-                console.log(error);
-              }}
-            />
-          </Flex>
-          <Flex justifyContent="center">
-            <Badge
-              fontSize="xl"
-              p={2}
-              borderRadius="md"
-              fontFamily="arabic"
-              border="1px solid"
-              borderColor="primary.500"
-            >
-              نُكَرِّسُ أَنفُسَنَا مِن أَجْلِ أَرْوَاحِ الآخَرِينَ
-            </Badge>
-          </Flex>
-          <MotionText
-            fontSize="l"
-            mb={5}
-            mt="2rem"
-            textAlign="center"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.9, duration: 0.5 }}
-          >
-            Embark on an exhilarating journey as we explore the realms of Cystic
-            Fibrosis together. Ready to dive in?
-          </MotionText>
-          <InterestingFacts />
-
-          <Box my={5}>
-            <TipsSection />
-          </Box>
-
-          <Box my={5}>
-            <Quiz />
-          </Box>
-          <Box my={5}>
-            <ResourcesSection />
-          </Box>
-        </MotionBox>
-      </Container>
-      <Flex
-        textAlign="center"
-        paddingTop="0.5rem"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Text fontSize="xs" fontFamily="arabic" style={{ direction: "rtl" }}>
-          © جميع حقوق النشر للنطاقات تحت candles.works تابعة لـ شموع البصري
-        </Text>
-        <Link href="https://github.com/HadiAlmarzooq" isExternal>
-          {/* <Icon as={FaGithub} mx={1} /> */}
-        </Link>
-      </Flex>
+          <Link href="https://github.com/HadiAlmarzooq" isExternal>
+            <Icon as={FaLinkedin} mx={1} />
+          </Link>
+          <Text fontSize="xs" fontFamily="arabic" style={{ direction: "rtl" }}>
+            © جميع حقوق النشر للنطاقات تحت candles.works تابعة لـ شموع البصري
+          </Text>
+        </Flex>
+      </Router>
     </ChakraProvider>
   );
 }
